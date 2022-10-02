9be2f5827ec5b1da823abd073edfcdcca062ec1f
kyddjam2live <48069421+kyddjam2live@users.noreply.github.com>
2022-10-02 08:33:59 -0400

    Add files via upload

---
+++ CreateMySqlDatabase.ps1
@@ -0,0 +1,361 @@
+﻿param(
+    [parameter(Mandatory = $false)]
+    $databaseName,
+
+    [parameter(Mandatory = $false)]
+    $databaseUserName,
+
+    [parameter(Mandatory = $false)]
+    $databaseUserPassword,
+
+    [parameter(Mandatory =$true)]
+    $databaseAdminUser,
+
+    [parameter(Mandatory = $true)]
+    $databaseAdminPassword,
+
+    [parameter(Mandatory = $false)]
+    $serverHostName,
+
+    [parameter(Mandatory = $false)]
+    $publishSettingFileName,
+
+    [parameter(Mandatory = $false)]
+    $publishSettingSavePath
+)
+
+Import-LocalizedData -BindingVariable Resources -FileName Resources.psd1
+
+#constants
+ $SCRIPTERROR = 0
+ $WARNING = 1
+ $INFO = 2
+ $logfile = ".\HostingLog-$(get-date -format MMddyyHHmmss).log"
+
+ $template = @"
+<?xml version="1.0" encoding="utf-8"?>
+<publishData>
+  <publishProfile
+    publishUrl=""
+    msdeploySite=""
+    destinationAppUrl=""
+    mySQLDBConnectionString=""
+    SQLServerDBConnectionString=""
+    profileName="Default Settings"
+    publishMethod="MSDeploy"
+    userName=""
+    userPWD=""
+    savePWD="True"
+    />
+</publishData>
+"@
+
+# this function does logging
+function write-log([int]$type, [string]$info){
+
+    $message = $info -f $args
+    $logMessage = get-date -format HH:mm:ss
+
+    Switch($type)
+    {
+        $SCRIPTERROR
+        {
+            $logMessage = $logMessage + "`t" + $Resources.Error + "`t" +  $message
+            write-host -foregroundcolor white -backgroundcolor red $logMessage
+        }
+        $WARNING
+        {
+            $logMessage = $logMessage + "`t" + $Resources.Warning + "`t" +  $message
+            write-host -foregroundcolor black -backgroundcolor yellow $logMessage
+        }
+        default
+        {
+            $logMessage = $logMessage + "`t" + $Resources.Info + "`t" +  $message
+            write-host -foregroundcolor black -backgroundcolor green  $logMessage
+        }
+    }
+
+    $logMessage >> $logfile
+}
+
+function GetPublishSettingSavePath()
+{
+    if(!$publishSettingFileName)
+    {
+        $publishSettingFileName = "WDeploy.PublishSettings"
+    }
+
+    if(!$publishSettingSavePath)
+    {
+        $publishSettingSavePath = [System.Environment]::GetFolderPath("Desktop")
+    }
+
+    if((test-path $publishSettingSavePath) -eq $false)
+    {
+        write-log $SCRIPTERROR $Resources.FailedToAccessScriptsFolder $publishSettingSavePath
+        return $null
+    }
+
+    return Join-Path $publishSettingSavePath $publishSettingFileName
+}
+
+$dbConnectionString = "server={0};uid={1};pwd={2}"
+$createDbScript = "Create database IF NOT EXISTS {0} "
+$createUserScript = "Grant all privileges on {0}.* to '{1}'@'localhost' identified by '{2}';"
+
+
+function GenerateStrongPassword()
+{
+   [System.Reflection.Assembly]::LoadWithPartialName("System.Web") > $null
+   return [System.Web.Security.Membership]::GeneratePassword(12,4)
+}
+
+function CreateProfileXml([string]$sqldbConnectionString, $pathToSaveFile)
+{
+    trap [Exception]
+    {
+        write-log $SCRIPTERROR $Resources.FailedToWritePublishSettingsFile $pathToSaveFile
+        return
+    }
+
+    $xml = New-Object xml
+
+    if(Test-Path $pathToSaveFile)
+    {
+        $xml.Load($pathToSaveFile)
+    }
+    else
+    {
+        $xml.LoadXml($template)
+    }
+
+    $newProfile = (@($xml.publishData.publishProfile)[0])
+    $newProfile.mySQLDBConnectionString = $sqldbConnectionString
+    $xml.Save($pathToSaveFile)
+
+    write-log $INFO $Resources.SavingPublishXmlToPath $pathToSaveFile
+}
+
+
+function SetupDatabase($hostname, $username, $adminUser, $adminPassword, $password, $dbName)
+{
+    trap [Exception]
+    {
+        write-log $SCRIPTERROR $Resources.FailedToSetupDatabase $dbName
+        return $false
+    }
+
+    $dbConnectionString = [string]::Format($dbConnectionString, $hostname, $adminUser, $adminPassword)
+    [System.Reflection.Assembly]::LoadWithPartialName('Mysql.Data')
+    $connection = New-Object MySql.Data.MySqlClient.MySqlConnection($dbConnectionString)
+    $connection.Open();
+
+    $createDbScript = [string]::Format($createDbScript, $dbName )
+    $command = New-Object MySql.Data.MySqlClient.MySqlCommand($createDbScript, $connection)
+    $command.ExecuteNonQuery()
+
+    $createUserScript = [string]::Format($createUserScript, $dbName, $username, $password)
+    $commandScript = New-Object MySql.Data.MySqlClient.MySqlCommand($createUserScript, $connection)
+    $commandScript.ExecuteNonQuery()
+
+    return $true
+}
+
+
+if(!$databaseUserName)
+{
+    $databaseUserName = "WDeployUser"
+}
+
+if(!$databaseUserPassword)
+{
+    $databaseUserPassword = GenerateStrongPassword
+}
+
+if(!$serverHostName)
+{
+    $serverHostName = "localhost"
+}
+
+if(!$databaseName)
+{
+    $databaseName = "WDeployDb"
+}
+
+if(($databaseAdminUser) -and ($databaseAdminPassword))
+{
+    if((SetupDatabase $serverHostName $databaseUserName $databaseAdminUser $databaseAdminPassword $databaseUserPassword $databaseName) -eq $true)
+    {
+        $connectionString = New-Object System.Data.Common.DbConnectionStringBuilder
+        $connectionString["server"] = "$serverHostName"
+        $connectionString["database"] = "$databaseName"
+        $connectionString["uid"] = "$databaseUserName"
+        $connectionString["pwd"] = "$databaseUserPassword"
+        $savePath = GetPublishSettingSavePath
+        if($savePath)
+        {
+            CreateProfileXml $connectionString.ConnectionString  $savePath
+        }
+    }
+}
+
+# SIG # Begin signature block
+# MIIdhQYJKoZIhvcNAQcCoIIddjCCHXICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUy4KpZdLIHZvuHK763HRKh1dp
+# xlegghhlMIIEwzCCA6ugAwIBAgITMwAAAMp9MhZ8fv0FAwAAAAAAyjANBgkqhkiG
+# 9w0BAQUFADB3MQswCQYDVQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQMA4G
+# A1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9uMSEw
+# HwYDVQQDExhNaWNyb3NvZnQgVGltZS1TdGFtcCBQQ0EwHhcNMTYwOTA3MTc1ODU1
+# WhcNMTgwOTA3MTc1ODU1WjCBszELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hp
+# bmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jw
+# b3JhdGlvbjENMAsGA1UECxMETU9QUjEnMCUGA1UECxMebkNpcGhlciBEU0UgRVNO
+# OjcyOEQtQzQ1Ri1GOUVCMSUwIwYDVQQDExxNaWNyb3NvZnQgVGltZS1TdGFtcCBT
+# ZXJ2aWNlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAj3CeDl2ll7S4
+# 96ityzOt4bkPI1FucwjpTvklJZLOYljFyIGs/LLi6HyH+Czg8Xd/oDQYFzmJTWac
+# A0flGdvk8Yj5OLMEH4yPFFgQsZA5Wfnz/Cg5WYR2gmsFRUFELCyCbO58DvzOQQt1
+# k/tsTJ5Ns5DfgCb5e31m95yiI44v23FVpKnTY9CUJbIr8j28O3biAhrvrVxI57GZ
+# nzkUM8GPQ03o0NGCY1UEpe7UjY22XL2Uq816r0jnKtErcNqIgglXIurJF9QFJrvw
+# uvMbRjeTBTCt5o12D4b7a7oFmQEDgg+koAY5TX+ZcLVksdgPNwbidprgEfPykXiG
+# ATSQlFCEXwIDAQABo4IBCTCCAQUwHQYDVR0OBBYEFGb30hxaE8ox6QInbJZnmt6n
+# G7LKMB8GA1UdIwQYMBaAFCM0+NlSRnAK7UD7dvuzK7DDNbMPMFQGA1UdHwRNMEsw
+# SaBHoEWGQ2h0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1Y3Rz
+# L01pY3Jvc29mdFRpbWVTdGFtcFBDQS5jcmwwWAYIKwYBBQUHAQEETDBKMEgGCCsG
+# AQUFBzAChjxodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpL2NlcnRzL01pY3Jv
+# c29mdFRpbWVTdGFtcFBDQS5jcnQwEwYDVR0lBAwwCgYIKwYBBQUHAwgwDQYJKoZI
+# hvcNAQEFBQADggEBAGyg/1zQebvX564G4LsdYjFr9ptnqO4KaD0lnYBECEjMqdBM
+# 4t+rNhN38qGgERoc+ns5QEGrrtcIW30dvMvtGaeQww5sFcAonUCOs3OHR05QII6R
+# XYbxtAMyniTUPwacJiiCSeA06tLg1bebsrIY569mRQHSOgqzaO52EzJlOtdLrGDk
+# Ot1/eu8E2zN9/xetZm16wLJVCJMb3MKosVFjFZ7OlClFTPk6rGyN9jfbKKDsDtNr
+# jfAiZGVhxrEqMiYkj4S4OyvJ2uhw/ap7dbotTCfZu1yO57SU8rE06K6j8zWB5L9u
+# DmtgcqXg3ckGvdmWVWBrcWgnmqNMYgX50XSzffQwggYHMIID76ADAgECAgphFmg0
+# AAAAAAAcMA0GCSqGSIb3DQEBBQUAMF8xEzARBgoJkiaJk/IsZAEZFgNjb20xGTAX
+# BgoJkiaJk/IsZAEZFgltaWNyb3NvZnQxLTArBgNVBAMTJE1pY3Jvc29mdCBSb290
+# IENlcnRpZmljYXRlIEF1dGhvcml0eTAeFw0wNzA0MDMxMjUzMDlaFw0yMTA0MDMx
+# MzAzMDlaMHcxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYD
+# VQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xITAf
+# BgNVBAMTGE1pY3Jvc29mdCBUaW1lLVN0YW1wIFBDQTCCASIwDQYJKoZIhvcNAQEB
+# BQADggEPADCCAQoCggEBAJ+hbLHf20iSKnxrLhnhveLjxZlRI1Ctzt0YTiQP7tGn
+# 0UytdDAgEesH1VSVFUmUG0KSrphcMCbaAGvoe73siQcP9w4EmPCJzB/LMySHnfL0
+# Zxws/HvniB3q506jocEjU8qN+kXPCdBer9CwQgSi+aZsk2fXKNxGU7CG0OUoRi4n
+# rIZPVVIM5AMs+2qQkDBuh/NZMJ36ftaXs+ghl3740hPzCLdTbVK0RZCfSABKR2YR
+# JylmqJfk0waBSqL5hKcRRxQJgp+E7VV4/gGaHVAIhQAQMEbtt94jRrvELVSfrx54
+# QTF3zJvfO4OToWECtR0Nsfz3m7IBziJLVP/5BcPCIAsCAwEAAaOCAaswggGnMA8G
+# A1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFCM0+NlSRnAK7UD7dvuzK7DDNbMPMAsG
+# A1UdDwQEAwIBhjAQBgkrBgEEAYI3FQEEAwIBADCBmAYDVR0jBIGQMIGNgBQOrIJg
+# QFYnl+UlE/wq4QpTlVnkpKFjpGEwXzETMBEGCgmSJomT8ixkARkWA2NvbTEZMBcG
+# CgmSJomT8ixkARkWCW1pY3Jvc29mdDEtMCsGA1UEAxMkTWljcm9zb2Z0IFJvb3Qg
+# Q2VydGlmaWNhdGUgQXV0aG9yaXR5ghB5rRahSqClrUxzWPQHEy5lMFAGA1UdHwRJ
+# MEcwRaBDoEGGP2h0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1
+# Y3RzL21pY3Jvc29mdHJvb3RjZXJ0LmNybDBUBggrBgEFBQcBAQRIMEYwRAYIKwYB
+# BQUHMAKGOGh0dHA6Ly93d3cubWljcm9zb2Z0LmNvbS9wa2kvY2VydHMvTWljcm9z
+# b2Z0Um9vdENlcnQuY3J0MBMGA1UdJQQMMAoGCCsGAQUFBwMIMA0GCSqGSIb3DQEB
+# BQUAA4ICAQAQl4rDXANENt3ptK132855UU0BsS50cVttDBOrzr57j7gu1BKijG1i
+# uFcCy04gE1CZ3XpA4le7r1iaHOEdAYasu3jyi9DsOwHu4r6PCgXIjUji8FMV3U+r
+# kuTnjWrVgMHmlPIGL4UD6ZEqJCJw+/b85HiZLg33B+JwvBhOnY5rCnKVuKE5nGct
+# xVEO6mJcPxaYiyA/4gcaMvnMMUp2MT0rcgvI6nA9/4UKE9/CCmGO8Ne4F+tOi3/F
+# NSteo7/rvH0LQnvUU3Ih7jDKu3hlXFsBFwoUDtLaFJj1PLlmWLMtL+f5hYbMUVbo
+# nXCUbKw5TNT2eb+qGHpiKe+imyk0BncaYsk9Hm0fgvALxyy7z0Oz5fnsfbXjpKh0
+# NbhOxXEjEiZ2CzxSjHFaRkMUvLOzsE1nyJ9C/4B5IYCeFTBm6EISXhrIniIh0EPp
+# K+m79EjMLNTYMoBMJipIJF9a6lbvpt6Znco6b72BJ3QGEe52Ib+bgsEnVLaxaj2J
+# oXZhtG6hE6a/qkfwEm/9ijJssv7fUciMI8lmvZ0dhxJkAj0tr1mPuOQh5bWwymO0
+# eFQF1EEuUKyUsKV4q7OglnUa2ZKHE3UiLzKoCG6gW4wlv6DvhMoh1useT8ma7kng
+# 9wFlb4kLfchpyOZu6qeXzjEp/w7FW1zYTRuh2Povnj8uVRZryROj/TCCBhEwggP5
+# oAMCAQICEzMAAACOh5GkVxpfyj4AAAAAAI4wDQYJKoZIhvcNAQELBQAwfjELMAkG
+# A1UEBhMCVVMxEzARBgNVBAgTCldhc2hpbmd0b24xEDAOBgNVBAcTB1JlZG1vbmQx
+# HjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEoMCYGA1UEAxMfTWljcm9z
+# b2Z0IENvZGUgU2lnbmluZyBQQ0EgMjAxMTAeFw0xNjExMTcyMjA5MjFaFw0xODAy
+# MTcyMjA5MjFaMIGDMQswCQYDVQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQ
+# MA4GA1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9u
+# MQ0wCwYDVQQLEwRNT1BSMR4wHAYDVQQDExVNaWNyb3NvZnQgQ29ycG9yYXRpb24w
+# ggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDQh9RCK36d2cZ61KLD4xWS
+# 0lOdlRfJUjb6VL+rEK/pyefMJlPDwnO/bdYA5QDc6WpnNDD2Fhe0AaWVfIu5pCzm
+# izt59iMMeY/zUt9AARzCxgOd61nPc+nYcTmb8M4lWS3SyVsK737WMg5ddBIE7J4E
+# U6ZrAmf4TVmLd+ArIeDvwKRFEs8DewPGOcPUItxVXHdC/5yy5VVnaLotdmp/ZlNH
+# 1UcKzDjejXuXGX2C0Cb4pY7lofBeZBDk+esnxvLgCNAN8mfA2PIv+4naFfmuDz4A
+# lwfRCz5w1HercnhBmAe4F8yisV/svfNQZ6PXlPDSi1WPU6aVk+ayZs/JN2jkY8fP
+# AgMBAAGjggGAMIIBfDAfBgNVHSUEGDAWBgorBgEEAYI3TAgBBggrBgEFBQcDAzAd
+# BgNVHQ4EFgQUq8jW7bIV0qqO8cztbDj3RUrQirswUgYDVR0RBEswSaRHMEUxDTAL
+# BgNVBAsTBE1PUFIxNDAyBgNVBAUTKzIzMDAxMitiMDUwYzZlNy03NjQxLTQ0MWYt
+# YmM0YS00MzQ4MWU0MTVkMDgwHwYDVR0jBBgwFoAUSG5k5VAF04KqFzc3IrVtqMp1
+# ApUwVAYDVR0fBE0wSzBJoEegRYZDaHR0cDovL3d3dy5taWNyb3NvZnQuY29tL3Br
+# aW9wcy9jcmwvTWljQ29kU2lnUENBMjAxMV8yMDExLTA3LTA4LmNybDBhBggrBgEF
+# BQcBAQRVMFMwUQYIKwYBBQUHMAKGRWh0dHA6Ly93d3cubWljcm9zb2Z0LmNvbS9w
+# a2lvcHMvY2VydHMvTWljQ29kU2lnUENBMjAxMV8yMDExLTA3LTA4LmNydDAMBgNV
+# HRMBAf8EAjAAMA0GCSqGSIb3DQEBCwUAA4ICAQBEiQKsaVPzxLa71IxgU+fKbKhJ
+# aWa+pZpBmTrYndJXAlFq+r+bltumJn0JVujc7SV1eqVHUqgeSxZT8+4PmsMElSnB
+# goSkVjH8oIqRlbW/Ws6pAR9kRqHmyvHXdHu/kghRXnwzAl5RO5vl2C5fAkwJnBpD
+# 2nHt5Nnnotp0LBet5Qy1GPVUCdS+HHPNIHuk+sjb2Ns6rvqQxaO9lWWuRi1XKVjW
+# kvBs2mPxjzOifjh2Xt3zNe2smjtigdBOGXxIfLALjzjMLbzVOWWplcED4pLJuavS
+# Vwqq3FILLlYno+KYl1eOvKlZbiSSjoLiCXOC2TWDzJ9/0QSOiLjimoNYsNSa5jH6
+# lEeOfabiTnnz2NNqMxZQcPFCu5gJ6f/MlVVbCL+SUqgIxPHo8f9A1/maNp39upCF
+# 0lU+UK1GH+8lDLieOkgEY+94mKJdAw0C2Nwgq+ZWtd7vFmbD11WCHk+CeMmeVBoQ
+# YLcXq0ATka6wGcGaM53uMnLNZcxPRpgtD1FgHnz7/tvoB3kH96EzOP4JmtuPe7Y6
+# vYWGuMy8fQEwt3sdqV0bvcxNF/duRzPVQN9qyi5RuLW5z8ME0zvl4+kQjOunut6k
+# LjNqKS8USuoewSI4NQWF78IEAA1rwdiWFEgVr35SsLhgxFK1SoK3hSoASSomgyda
+# Qd691WZJvAuceHAJvDCCB3owggVioAMCAQICCmEOkNIAAAAAAAMwDQYJKoZIhvcN
+# AQELBQAwgYgxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYD
+# VQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xMjAw
+# BgNVBAMTKU1pY3Jvc29mdCBSb290IENlcnRpZmljYXRlIEF1dGhvcml0eSAyMDEx
+# MB4XDTExMDcwODIwNTkwOVoXDTI2MDcwODIxMDkwOVowfjELMAkGA1UEBhMCVVMx
+# EzARBgNVBAgTCldhc2hpbmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoT
+# FU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEoMCYGA1UEAxMfTWljcm9zb2Z0IENvZGUg
+# U2lnbmluZyBQQ0EgMjAxMTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIB
+# AKvw+nIQHC6t2G6qghBNNLrytlghn0IbKmvpWlCquAY4GgRJun/DDB7dN2vGEtgL
+# 8DjCmQawyDnVARQxQtOJDXlkh36UYCRsr55JnOloXtLfm1OyCizDr9mpK656Ca/X
+# llnKYBoF6WZ26DJSJhIv56sIUM+zRLdd2MQuA3WraPPLbfM6XKEW9Ea64DhkrG5k
+# NXimoGMPLdNAk/jj3gcN1Vx5pUkp5w2+oBN3vpQ97/vjK1oQH01WKKJ6cuASOrdJ
+# Xtjt7UORg9l7snuGG9k+sYxd6IlPhBryoS9Z5JA7La4zWMW3Pv4y07MDPbGyr5I4
+# ftKdgCz1TlaRITUlwzluZH9TupwPrRkjhMv0ugOGjfdf8NBSv4yUh7zAIXQlXxgo
+# tswnKDglmDlKNs98sZKuHCOnqWbsYR9q4ShJnV+I4iVd0yFLPlLEtVc/JAPw0Xpb
+# L9Uj43BdD1FGd7P4AOG8rAKCX9vAFbO9G9RVS+c5oQ/pI0m8GLhEfEXkwcNyeuBy
+# 5yTfv0aZxe/CHFfbg43sTUkwp6uO3+xbn6/83bBm4sGXgXvt1u1L50kppxMopqd9
+# Z4DmimJ4X7IvhNdXnFy/dygo8e1twyiPLI9AN0/B4YVEicQJTMXUpUMvdJX3bvh4
+# IFgsE11glZo+TzOE2rCIF96eTvSWsLxGoGyY0uDWiIwLAgMBAAGjggHtMIIB6TAQ
+# BgkrBgEEAYI3FQEEAwIBADAdBgNVHQ4EFgQUSG5k5VAF04KqFzc3IrVtqMp1ApUw
+# GQYJKwYBBAGCNxQCBAweCgBTAHUAYgBDAEEwCwYDVR0PBAQDAgGGMA8GA1UdEwEB
+# /wQFMAMBAf8wHwYDVR0jBBgwFoAUci06AjGQQ7kUBU7h6qfHMdEjiTQwWgYDVR0f
+# BFMwUTBPoE2gS4ZJaHR0cDovL2NybC5taWNyb3NvZnQuY29tL3BraS9jcmwvcHJv
+# ZHVjdHMvTWljUm9vQ2VyQXV0MjAxMV8yMDExXzAzXzIyLmNybDBeBggrBgEFBQcB
+# AQRSMFAwTgYIKwYBBQUHMAKGQmh0dHA6Ly93d3cubWljcm9zb2Z0LmNvbS9wa2kv
+# Y2VydHMvTWljUm9vQ2VyQXV0MjAxMV8yMDExXzAzXzIyLmNydDCBnwYDVR0gBIGX
+# MIGUMIGRBgkrBgEEAYI3LgMwgYMwPwYIKwYBBQUHAgEWM2h0dHA6Ly93d3cubWlj
+# cm9zb2Z0LmNvbS9wa2lvcHMvZG9jcy9wcmltYXJ5Y3BzLmh0bTBABggrBgEFBQcC
+# AjA0HjIgHQBMAGUAZwBhAGwAXwBwAG8AbABpAGMAeQBfAHMAdABhAHQAZQBtAGUA
+# bgB0AC4gHTANBgkqhkiG9w0BAQsFAAOCAgEAZ/KGpZjgVHkaLtPYdGcimwuWEeFj
+# kplCln3SeQyQwWVfLiw++MNy0W2D/r4/6ArKO79HqaPzadtjvyI1pZddZYSQfYtG
+# UFXYDJJ80hpLHPM8QotS0LD9a+M+By4pm+Y9G6XUtR13lDni6WTJRD14eiPzE32m
+# kHSDjfTLJgJGKsKKELukqQUMm+1o+mgulaAqPyprWEljHwlpblqYluSD9MCP80Yr
+# 3vw70L01724lruWvJ+3Q3fMOr5kol5hNDj0L8giJ1h/DMhji8MUtzluetEk5CsYK
+# wsatruWy2dsViFFFWDgycScaf7H0J/jeLDogaZiyWYlobm+nt3TDQAUGpgEqKD6C
+# PxNNZgvAs0314Y9/HG8VfUWnduVAKmWjw11SYobDHWM2l4bf2vP48hahmifhzaWX
+# 0O5dY0HjWwechz4GdwbRBrF1HxS+YWG18NzGGwS+30HHDiju3mUv7Jf2oVyW2ADW
+# oUa9WfOXpQlLSBCZgB/QACnFsZulP0V3HjXG0qKin3p6IvpIlR+r+0cjgPWe+L9r
+# t0uX4ut1eBrs6jeZeRhL/9azI2h15q/6/IvrC4DqaTuv/DDtBEyO3991bWORPdGd
+# Vk5Pv4BXIqF4ETIheu9BCrE/+6jMpF3BoYibV3FWTkhFwELJm3ZbCoBIa/15n8G9
+# bW1qyVJzEw16UM0xggSKMIIEhgIBATCBlTB+MQswCQYDVQQGEwJVUzETMBEGA1UE
+# CBMKV2FzaGluZ3RvbjEQMA4GA1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9z
+# b2Z0IENvcnBvcmF0aW9uMSgwJgYDVQQDEx9NaWNyb3NvZnQgQ29kZSBTaWduaW5n
+# IFBDQSAyMDExAhMzAAAAjoeRpFcaX8o+AAAAAACOMAkGBSsOAwIaBQCggZ4wGQYJ
+# KoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQB
+# gjcCARUwIwYJKoZIhvcNAQkEMRYEFA5bZLjANP5v4rpy3Pc1vwAKOq9vMD4GCisG
+# AQQBgjcCAQwxMDAuoBaAFABXAGUAYgAgAEQAZQBwAGwAbwB5oRSAEmh0dHA6Ly93
+# d3cuaWlzLm5ldDANBgkqhkiG9w0BAQEFAASCAQCTlmfijo7FryUAMeqAoQoaXnPu
+# Cf2KiIZuLSkWU01wBe+qXLghw7P+kXW2EEIkvjgcxTV0+C8rZoJAY9A//KbHpTwq
+# GBRmYoGT6ptv2qz/0+BTPjZKHs7mzFww2u3QA1YT2P8MTwJ7xC4KjU4fX2E4HHkD
+# E95lNR8QwJU31KijBmMWyU9mMa2sa29GUWsFFcCq4mG9AOx8jWKcg/OFXHlpIx/i
+# lydT1vFpg7Gf96oxygkY/PAtiphKvxC6jbGgct/leiR6Jw1Rdlq62ZHO3YMf3C74
+# T/PfK/zh21wqp/H/oiudlDcsN7NfWAjuzvx//byd+Etc7Tdu1HutnQSXKqMIoYIC
+# KDCCAiQGCSqGSIb3DQEJBjGCAhUwggIRAgEBMIGOMHcxCzAJBgNVBAYTAlVTMRMw
+# EQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYDVQQHEwdSZWRtb25kMR4wHAYDVQQKExVN
+# aWNyb3NvZnQgQ29ycG9yYXRpb24xITAfBgNVBAMTGE1pY3Jvc29mdCBUaW1lLVN0
+# YW1wIFBDQQITMwAAAMp9MhZ8fv0FAwAAAAAAyjAJBgUrDgMCGgUAoF0wGAYJKoZI
+# hvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTcwNDI4MDExNTAw
+# WjAjBgkqhkiG9w0BCQQxFgQUBN4yaS5LE7k0dUBy8oBGCYlBrt0wDQYJKoZIhvcN
+# AQEFBQAEggEAMbQpgIlGdcizAx705zyQxCpBVI+FIcISwJL4iG8FpCtNKtihg4/T
+# DzWxuXd812FCcOjiNc1XJAat2MHrc83hFDA2Mt0QBatEHNi1r3Zr2zn8Hi1VuBXN
+# wPybGSWw4UDzfxp5k5QkRGlJxy8bu7ZOgJU6Z9a3ZugMa/laLNV6J+kGK+0YCKFJ
+# qE0THRtPUIsl8KLBHBXmtXXMS6neLLE+hwVg/C11XrbXge50HWGI+jg98qKSarGc
+# lfEfFWeZhBTHm07Xl9UoMh/TJee587wZa4VZ8+zgmP6OKIeLZmpq8Nc1n0tGrfnq
+# r6CDwuSIGcsTRXK1IdTBLniNnM0gZvO8FQ==
+# SIG # End signature block
---
+++ Resources.psd1
@@ -0,0 +1,88 @@
+﻿ConvertFrom-StringData @'
+
+Info=Información
+
+Warning=Advertencia
+
+Error=Error
+
+GrantedPermissions=Permisos de {0} concedidos a {1} en la ruta de acceso {2}.
+
+NotGrantedPermissions=No se pudieron conceder permisos de {0} a {1} en la ruta de acceso {2}.
+
+IsAdmin=Se confirmó que {0} es un miembro del grupo local Administradores.
+
+CreatedUser=No se pudo encontrar una cuenta de usuario existente. Se creó el usuario local {0}.
+
+AddedUserAsAdmin=Se agregó {0} al grupo local Administradores.
+
+CheckIIS7Installed=No se pudo cargar Microsoft.Web.*.dll.  Compruebe que IIS 7 está instalado.
+
+RuleNotCreated=Se pasó por alto la creación de la regla para {0} porque ya existe.
+
+CreatedRule=Se creó la regla de delegación para proveedores: {0}.
+
+NotServerOS=SKU actual no válido. Este script solamente se debe usar en un SKU de servidor.
+
+WDeployNotInstalled=Web Deploy debe estar instalado antes de ejecutar este script.
+
+HandlerNotInstalled=La característica Controlador de implementación de IIS 7 de Web Deploy debe estar instalada. Agregue esta característica mediante Agregar o quitar programas.
+
+SharedConfigInUse=No se puede ejecutar este script cuando la configuración compartida está habilitada.
+
+NoPasswordForGivenUser=Se necesita una contraseña si se especifica un usuario. Especifique la contraseña para {0} e inténtelo de nuevo.
+
+PasswordWillBeReset=No se especificó una contraseña para {0}. La contraseña de usuario se restablecerá porque está establecida la opción para pasar por alto los errores de restablecimiento.
+
+FailedToValidateUserWithSpecifiedPassword=No se pudo validar el usuario {0} con la contraseña especificada.
+
+UpdatedRunAsForSpecificUser=Se actualizó la contraseña para el usuario runAs {1} que se especificó en la regla para {0}
+
+SiteCreationFailed=No se pudo crear el sitio. El script se cerrará ahora.
+
+FailedToGrantUserAccessToSite=No se pudieron conceder permisos del Administrador de IIS para {0} en el sitio {1}.
+
+GrantedUserAccessToSite=Permisos del Administrador de IIS concedidos para {0} en el sitio {1}.
+
+UserHasAccessToSite=Se confirmó que {0} tiene permisos del Administrador de IIS para el sitio {1}.
+
+FailedToAccessScriptsFolder=No se pudo obtener acceso a la ubicación de almacenamiento de los archivos de configuración de publicación: {0}. La configuración no se guardará.
+
+SavingPublishXmlToPath=Configuración de publicación guardada en {0}
+
+FailedToWritePublishSettingsFile=No se pudo crear el archivo de configuración de publicación en {0}.
+
+AppPoolCreated=Grupo de aplicaciones {0} creado.
+
+AppPoolExists=El grupo de aplicaciones {0} ya existe. Otras aplicaciones pueden estar usando el grupo de aplicaciones. Es recomendable tener un grupo de aplicaciones por sitio o deshabilitar todas las reglas de delegación de appPoolPipeline, appPoolNetFx o recycleApp. 
+
+SiteCreated=Sitio {0} creado.
+
+SiteAppPoolUpdated=Se confirmó que el sitio {0} existe. El grupo de aplicaciones para el sitio cambió a {1}.
+
+SiteExists=Se confirmó que el sitio {0} existe y utiliza el grupo de aplicaciones {1}.
+
+SiteVirtualDirectoryExists=Omitiendo la creación del directorio del sitio porque el directorio {0} ya existe.  Puede existir contenido en este directorio.
+
+FailedToCreateLogin=No se pudo crear el inicio de sesión {0}
+
+LoginExists=El inicio de sesión {0} ya existe.
+
+FailedToCreateDbUser=No se pudo crear el usuario {0}.
+
+DbUserExists=El usuario de base de datos {0} ya existe.
+
+FailedToSetupDatabase=No se pudo configurar la base de datos {0}.
+
+FailedToCreateDatabase=No se pudo crear la base de datos {0}.
+
+DbExists=La base de datos {0} ya existe.
+
+SmoNotInstalled=No se pudo crear la base de datos. Asegúrese de que Objetos de administración de Microsoft SQL Server (versión 10 o superior) está instalado.
+
+NoPasswordForExistingUserForPublish=No se especificó la contraseña de usuario de implementación y no se guardará en el archivo de configuración de publicación.
+
+FailedToGetPasswordFromRegistry=No se puede recuperar la contraseña de {0} del Registro. Especifique la contraseña de administrador de base de datos e inténtelo de nuevo.
+
+'@ 
+
